4c31ff21fd5c9e8564ff448f1a121f5d1518154c
AntonOcean <tmath90@yandex.ru>
2018-11-10 19:04:27 +0300

    Create admin

--- blog/backend/admin.py
+++ blog/backend/admin.py
@@ -2,4 +2,41 @@ from django.contrib import admin
 
 from backend.models import Question, Tag, Answer, Profile
 
-admin.site.register((Question, Profile, Tag, Answer))
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('created', 'author', 'rating', 'question')
+    readonly_fields = ('created', 'rating')
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created', 'rating')
+    readonly_fields = ('created', 'rating', 'count_answers')
+    list_filter = ('tags', 'author')
+
+    inlines = [AnswerInline]
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question.tags.through
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', )
+    readonly_fields = ('rating', )
+#     add fields
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rating')
+    readonly_fields = ('rating',)
+
+    inlines = [QuestionInline]
+    exclude = ('tags',)
